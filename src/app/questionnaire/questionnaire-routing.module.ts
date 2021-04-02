946c0df26d982a7cb946ed08aeb3bf15df4ce4a0
Alexandr <XoxlofAlexandr@yandex.ru>
2021-04-02 20:56:50 +0300

    добавлены шрифты, добавлена страница списка анкет

--- src/app/questionnaire/questionnaire-routing.module.ts
+++ src/app/questionnaire/questionnaire-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionnairesListComponent } from './pages/questionnaires-list/questionnaires-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: QuestionnairesListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class QuestionnaireRoutingModule { }
+export class QuestionnaireRoutingModule {}
