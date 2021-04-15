0f0b31139dcebc503ea0e52b0e584166f2732b80
Alexandr <XoxlofAlexandr@yandex.ru>
2021-04-15 13:40:56 +0300

    сверстаны секции локации и часто задаваемых вопросов лендинга

--- src/app/home/pages/index/index.component.ts
+++ src/app/home/pages/index/index.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FaqService } from 'src/app/shared/services/faq.service';
+import { FaqInterface } from 'src/app/shared/types/faq.interface';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+  public faq$: Observable<FaqInterface[]>;
 
-  constructor() { }
+  constructor(private faq: FaqService) {}
 
   ngOnInit(): void {
+    this.faq$ = this.faq.getFaq();
   }
-
 }
---
+++ src/app/shared/types/faq.interface.ts
@@ -0,0 +1,5 @@
+export interface FaqInterface {
+  id: number;
+  title: string;
+  text: string;
+}
