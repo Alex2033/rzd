d61a75a6ca1015f8f481b2fba6b2e7e6898fae5c
Alexandr <XoxlofAlexandr@yandex.ru>
2021-04-16 11:25:07 +0300

    сверстана детальная страница услуг(без табов)

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
+import { ServiceDetailComponent } from './pages/service-detail/service-detail.component';
 import { ServicePointsComponent } from './pages/service-points/service-points.component';
 import { ServicesComponent } from './pages/services/services.component';
 
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'services',
     component: ServicesComponent,
+  },
+  {
+    path: 'services/:id',
+    component: ServiceDetailComponent,
   },
   {
     path: 'service-points',
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,17 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { FooterComponent } from '../shared/layout/footer/footer.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ServicePointsComponent } from './pages/service-points/service-points.component';
+import { ServiceDetailComponent } from './pages/service-detail/service-detail.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [IndexComponent, FooterComponent, ServicesComponent, ServicePointsComponent],
-  imports: [CommonModule, HomeRoutingModule, MatExpansionModule],
+  declarations: [
+    IndexComponent,
+    FooterComponent,
+    ServicesComponent,
+    ServicePointsComponent,
+    ServiceDetailComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule, MatExpansionModule, MatTabsModule],
 })
 export class HomeModule {}
