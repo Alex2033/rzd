952c09985f4b352285455387987b9b926814552a
Alexandr <XoxlofAlexandr@yandex.ru>
2021-05-13 15:20:07 +0300

    изменены ссылки в пользовательском меню, добавлен редирект в логин после логаута

--- src/app/shared/layout/user-menu/user-menu.component.ts
+++ src/app/shared/layout/user-menu/user-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { MenuService } from '../../services/menu.service';
 
@@ -8,7 +9,11 @@ import { MenuService } from '../../services/menu.service';
   styleUrls: ['./user-menu.component.scss'],
 })
 export class UserMenuComponent implements OnInit {
-  constructor(private menu: MenuService, private auth: AuthService) {}
+  constructor(
+    private menu: MenuService,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
@@ -19,5 +24,6 @@ export class UserMenuComponent implements OnInit {
   logout(): void {
     this.closeMenu();
     this.auth.logout();
+    this.router.navigate(['/auth', 'login']);
   }
 }
