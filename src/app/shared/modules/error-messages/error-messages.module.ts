261a2b2ab8fb15d17210e604a5780aed6fef5b8b
Alexandr <XoxlofAlexandr@yandex.ru>
2021-05-16 20:51:49 +0300

    добавлен CommonModule

--- src/app/shared/modules/error-messages/error-messages.module.ts
+++ src/app/shared/modules/error-messages/error-messages.module.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ErrorMessagesComponent } from './error-messages.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [ErrorMessagesComponent],
   exports: [ErrorMessagesComponent],
 })
