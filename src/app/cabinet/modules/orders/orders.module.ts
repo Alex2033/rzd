0c48ca7780496b4e6763a20b6acde92da6240760
Alexandr <XoxlofAlexandr@yandex.ru>
2021-06-07 18:54:06 +0300

    добавлен плагин герерации штрих-кода

--- src/app/cabinet/modules/orders/orders.module.ts
+++ src/app/cabinet/modules/orders/orders.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { OrdersRoutingModule } from './orders-routing.module';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgxBarcodeModule } from 'ngx-barcode';
+
 import { OrdersListComponent } from './pages/orders-list/orders-list.component';
 import { OrderDetailComponent } from './pages/order-detail/order-detail.component';
 import { DocumentsComponent } from './pages/documents/documents.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { HowGetResultsComponent } from './pages/how-get-results/how-get-results.component';
 
 @NgModule({
-  declarations: [OrdersListComponent, OrderDetailComponent, DocumentsComponent, HowGetResultsComponent],
+  declarations: [
+    OrdersListComponent,
+    OrderDetailComponent,
+    DocumentsComponent,
+    HowGetResultsComponent,
+  ],
   imports: [
     CommonModule,
     OrdersRoutingModule,
     MatSelectModule,
     MatFormFieldModule,
+    NgxBarcodeModule,
   ],
 })
 export class OrdersModule {}
