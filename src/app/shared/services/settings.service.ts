d2dee49836169e0d081922cdb5fd862d1b71c8f8
Alexandr <XoxlofAlexandr@yandex.ru>
2021-07-22 12:51:23 +0300

    добавлен сервис получения настроек

---
+++ src/app/shared/services/settings.service.ts
@@ -0,0 +1,18 @@
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SettingsInterface } from '../types/settings.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+  constructor(private http: HttpClient) {}
+
+  getSettings(): Observable<SettingsInterface> {
+    return this.http.get<SettingsInterface>(
+      environment.api + 'api/contents/settings'
+    );
+  }
+}
---
+++ src/app/shared/types/settings.interface.ts
@@ -0,0 +1,5 @@
+export interface SettingsInterface {
+  enableOnlinePayment: boolean;
+  enableCorpPayment: boolean;
+  enableOrderCreation: boolean;
+}
