accbfa469c1cb4a284389c6da9cdea2cfc4f2043
Alexandr <XoxlofAlexandr@yandex.ru>
2021-07-26 12:09:34 +0300

    обновлен интерфейс настроек

--- src/app/shared/types/settings.interface.ts
+++ src/app/shared/types/settings.interface.ts
@@ -2,4 +2,15 @@ export interface SettingsInterface {
   enableOnlinePayment: boolean;
   enableCorpPayment: boolean;
   enableOrderCreation: boolean;
+  utm_discounts: UtmDiscountsInterface[];
+}
+
+interface UtmDiscountsInterface {
+  id: string;
+  utm_source: string;
+  value: string;
+  type: string;
+  dt_start: string;
+  dt_stop: string;
+  enabled: boolean;
 }
