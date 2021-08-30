c0494d24a05fe471bf8141c1a4fd8bdcc60b39de
Alexandr <XoxlofAlexandr@yandex.ru>
2021-08-30 22:51:58 +0300

    feat: убраны лишние пункты футера, добавлены документы для новосибирска и мск на страницу юр. информации

--- src/app/home/pages/legal-information/legal-information.component.ts
+++ src/app/home/pages/legal-information/legal-information.component.ts
@@ -1,15 +1,72 @@
+import { LocationService } from 'src/app/shared/services/location.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-legal-information',
   templateUrl: './legal-information.component.html',
-  styleUrls: ['./legal-information.component.scss']
+  styleUrls: ['./legal-information.component.scss'],
 })
 export class LegalInformationComponent implements OnInit {
+  public docs: object[] = [];
+  public moscowDocs: object[] = [
+    {
+      name: 'Реквизиты',
+      file: 'assets/files/kartochka-rekvizitov-tskb-alfa-bank-s-31-03-21.pdf',
+    },
+    {
+      name: 'Договор оферты',
+      file: 'assets/files/Приложение_№1_к_приказу_ПУБЛИЧНАЯ_ОФЕРТА_июль.pdf',
+    },
+    {
+      name: 'Приказ № 253-ОД от 09.06.2021',
+      file: 'assets/files/Приказ № 253-ОД от 09.06.2021.pdf',
+    },
+    {
+      name: 'Согласие пациента на обработку персональных данных',
+      file: 'assets/files/Согласие_пациента_на_обработку_персональных_данных_шаблон.docx',
+    },
+    {
+      name: 'Лицензия на медицинскую деятельность',
+      file: 'assets/files/1.Лицензия на медицинскую деятельность ЧУЗ ЦКБ РЖД-Медицина от 27.07.2020г. (новейшая).pdf',
+    },
+  ];
 
-  constructor() { }
+  public novosibDocs: object[] = [
+    {
+      name: 'Реквизиты',
+      file: 'assets/files/2021 реквизиты ЧУЗ КБ Новосибирск.pdf',
+    },
+    {
+      name: 'Договор оферты',
+      file: 'assets/files/Приложение №1 к приказу ПУБЛИЧНАЯ ОФЕРТА.docx',
+    },
+    {
+      name: 'Приказ № 618 от 23.08.2021',
+      file: 'assets/files/Приказ № 618 от 23.08.2021_публичная оферта.pdf',
+    },
+    {
+      name: 'Согласие пациента на обработку персональных данных',
+      file: 'assets/files/Согласие_пациента_на_обработку_персональных_данных_шаблон с ЕМИАС-2.docx',
+    },
+    {
+      name: 'Лицензия на медицинскую деятельность',
+      file: 'assets/files/Лицензия_Новосибирск.pdf',
+    },
+    {
+      name: 'Политика обработки персональных данных',
+      file: 'assets/files/401SCANDOC30062017.pdf',
+    },
+  ];
+
+  constructor(private location: LocationService) {}
 
   ngOnInit() {
+    this.location.currentLocation$.subscribe((location) => {
+      if (location.id === 1101) {
+        this.docs = [...this.moscowDocs];
+      } else {
+        this.docs = [...this.novosibDocs];
+      }
+    });
   }
-
 }
