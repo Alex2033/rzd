fd534695623e7eeff3b9ca75fb6fe6e23f1bb0bb
Alexandr <XoxlofAlexandr@yandex.ru>
2021-09-11 12:00:42 +0300

    refactor(payment-method): убрана ограничение онлайн оплаты для Новосибирска

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,17 @@ import { EnvironmentInterface } from 'src/app/shared/types/environment.interface
 export const environment: EnvironmentInterface = {
   production: false,
   api: '/',
-  // todo: потом убрать
+
+  /* TEST */
+  // defaultLocation: {
+  //   id: 1101,
+  //   ur_id: 1082,
+  //   name: 'Москва',
+  //   latitude: 55.7539,
+  //   longtitude: 37.6358,
+  // },
+
+  /* PROD */
   defaultLocation: {
     id: 1122,
     ur_id: 1120,
