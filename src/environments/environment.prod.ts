248175c6d04829f0577c0041b003ec4509fb0766
Alexandr <XoxlofAlexandr@yandex.ru>
2021-09-23 22:52:52 +0300

    feat(footer): добавлен документ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,20 +5,20 @@ export const environment: EnvironmentInterface = {
   api: '/',
 
   /* TEST */
-  // defaultLocation: {
-  //   id: 1101,
-  //   ur_id: 1082,
-  //   name: 'Москва',
-  //   latitude: 55.7539,
-  //   longtitude: 37.6358,
-  // },
+  defaultLocation: {
+    id: 1101,
+    ur_id: 1082,
+    name: 'Москва',
+    latitude: 55.7539,
+    longtitude: 37.6358,
+  },
 
   /* PROD */
-  defaultLocation: {
-    id: 1122,
-    ur_id: 1120,
-    name: 'Москва',
-    latitude: 55.75322,
-    longtitude: 37.622513,
-  },
+  // defaultLocation: {
+  //   id: 1122,
+  //   ur_id: 1120,
+  //   name: 'Москва',
+  //   latitude: 55.75322,
+  //   longtitude: 37.622513,
+  // },
 };
