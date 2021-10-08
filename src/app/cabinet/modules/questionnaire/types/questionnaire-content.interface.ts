704b3cc7f0d8b77be6af821ff223d7fa343628d0
Alexandr <XoxlofAlexandr@yandex.ru>
2021-10-08 13:31:19 +0300

    refactor(questionnaire interface): удалены неиспользуемые поля

--- src/app/cabinet/modules/questionnaire/types/questionnaire-content.interface.ts
+++ src/app/cabinet/modules/questionnaire/types/questionnaire-content.interface.ts
@@ -1,8 +1,6 @@
 export interface QuestionnaireContentInterface {
   name: string;
-  name_lat: string;
   surname: string;
-  surname_lat: string;
   patronymic: string;
   birthday: string;
   email: string;
@@ -18,18 +16,12 @@ export interface QuestionnaireContentInterface {
   snils: string;
   adress_single: string;
   adress_reg_country: string;
-  adress_reg_region: string;
-  adress_reg_area: string;
   adress_reg_city: string;
-  adress_reg_location: string;
   adress_reg_street: string;
   adress_reg_building: string;
   adress_reg_flat: string;
   adress_fact_country: string;
-  adress_fact_region: string;
-  adress_fact_area: string;
   adress_fact_city: string;
-  adress_fact_location: string;
   adress_fact_street: string;
   adress_fact_building: string;
   adress_fact_flat: string;
